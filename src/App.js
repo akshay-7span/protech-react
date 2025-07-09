bea84e54eda34df56b300af99108a030a79f3155
Vaibhav Savaliya <vaibhav.s@7span.com>
2025-07-09 15:20:47 +0530

    Added Home page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import LoginPage from './LoginPage';
 import LogoutPage from './LogoutPage';
 import { Route, Router, Routes } from 'react-router-dom';
+import Home from './Home';
 
 function App() {
 
   return (
       <Routes>
         <Route path="/" element={<LoginPage />} />       {/* http://localhost:3000/ */}
+        <Route path="/home" element={<Home />} />       {/* http://localhost:3000/ */}
         <Route path="/logout" element={<LogoutPage />} /> {/* http://localhost:3000/about */}
         {/* <Route path="*" element={<NotFound />} />    */}
       </Routes>
--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -10,6 +10,24 @@ const LoginPage = () => {
         window.location.href = "https://my.easa.com/Portals/_default/Skins/MXOnline/pages/SignOut.aspx?returnurl=http%3A%2F%2Flocalhost%3A3000%2Flogout?ctl=logoff"
     }
 
+    const fetchUserDetails = () => {
+        fetch('https://api--staging.testiqo.com/registered-users', {
+            method: 'GET',
+            // credentials: 'include',
+            headers: {
+                // 'Content-Type': 'text/plain',
+                'authToken' : 'd5131fde-f5af-4ad9-b5e3-ca41123ccfc9'
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                document.getElementById('user_details').innerHTML = JSON.stringify(data);
+                console.log('User Details:', data);
+            })
+            .catch(error => {
+                console.error('Error fetching user details:', error);
+            });
+    }
 
     return (
         <div className="App">
@@ -24,6 +42,8 @@ const LoginPage = () => {
                 <div>
                     {/* <Button variant="contained" onClick={() => loginApiCall()} style={{margin: 10}}>Login To EASA</Button> */}
                     {/* <Button variant="contained" onClick={() => logoutApiCall()} style={{ margin: 10 }}>Fetch User Details</Button> */}
+                    <Button variant="contained" onClick={() => fetchUserDetails()} style={{ margin: 10 }}>Fetch User Details</Button>
+                                        
                     <Button variant="contained" onClick={() => logoutApiCall()} style={{ margin: 10 }}>Logout From EASA</Button>
                     {/* <Button variant="contained" onClick={() => protechApiCall()}>Calling auth-status api</Button> */}
 
