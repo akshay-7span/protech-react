8bdce0e237f5dad24dc3832267985ab1d4ed20a3
Vaibhav Savaliya <vaibhav.s@7span.com>
2025-07-09 16:33:34 +0530

    Added HashRouter

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import LoginPage from './LoginPage';
 import LogoutPage from './LogoutPage';
 import { Route, Router, Routes } from 'react-router-dom';
 import Home from './Home';
+import { HashRouter } from 'react-router-dom';
 
 function App() {
 
   return (
+    <HashRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />       {/* http://localhost:3000/ */}
         <Route path="/home" element={<Home />} />       {/* http://localhost:3000/ */}
         <Route path="/logout" element={<LogoutPage />} /> {/* http://localhost:3000/about */}
         {/* <Route path="*" element={<NotFound />} />    */}
       </Routes>
+    </HashRouter>
   );
 }
 
