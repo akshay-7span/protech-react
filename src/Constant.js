51a8f9173972c29cd52180fa2093663ff5aca71f
Vaibhav Savaliya <vaibhav.s@7span.com>
2025-07-09 19:37:55 +0530

    change endpoints

--- src/Constant.js
+++ src/Constant.js
@@ -1,8 +1,8 @@
 export const FETCH_USER_DETAILS = "https://api--staging.testiqo.com/registered-users";
 
-export const LOCALHOST_DEFAULT_PAGE_URL = "http://localhost:3000/#";
-export const LOCALHOST_DEFAULT_HOME_URL = "http://localhost:3000/#/home";
+// export const LOCALHOST_DEFAULT_PAGE_URL = "http://localhost:3000/#";
+// export const LOCALHOST_DEFAULT_HOME_URL = "http://localhost:3000/#/home";
 
 
-// export const LOCALHOST_DEFAULT_PAGE_URL = "https%3A%2F%2Fdemo-app-oc4ai.ondigitalocean.app%2F%23";
-// export const LOCALHOST_DEFAULT_HOME_URL = "https%3A%2F%2Fdemo-app-oc4ai.ondigitalocean.app%2F#%2Fhome";
+export const LOCALHOST_DEFAULT_PAGE_URL = "https://demo-app-oc4ai.ondigitalocean.app/#";
+export const LOCALHOST_DEFAULT_HOME_URL = "https://demo-app-oc4ai.ondigitalocean.app/#/home";
